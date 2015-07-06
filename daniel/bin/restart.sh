e9ccf37a676965a0f8c0ebd2e6e4b369dd2c5f08
Daniel Shin <swshin518@hotmail.com>
2015-07-06 12:43:06 +0800

    INITIAL COMMIT: first fork commit

---
+++ daniel/bin/restart.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export HEROKU_APP_NAME='prerender-server'
+export HEROKU_EMAIL='swshin518@hotmail.com'
+export HEROKU_API_TOKEN='a6c8e653-e31e-482c-9661-de346dbb2df7'
+
+node ./bin/restart-heroku
